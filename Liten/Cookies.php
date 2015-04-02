06aaa84753d77c6061cf02e99bcb63b2aca158af
Joshua parker <josh@7mediaws.org>
2015-04-02 00:41:19 -0400

    Added better secure checking of cookies with the new hash_equals() function.

--- Liten/Cookies.php
+++ Liten/Cookies.php
@@ -40,23 +40,7 @@ class Cookies
      */
     protected $_app;
 
-    /**
-     * Regular expression for matching and validating a MAC address
-     * @var string
-     */
-    protected $_valid_mac = "([0-9A-F]{2}[:-]){5}([0-9A-F]{2})";
-
-    /**
-     * An array of valid MAC address characters
-     * @var array
-     * @formatter:off
-     */
-    protected $_mac_address_vals = [
-        "0", "1", "2", "3", "4", "5", "6", "7",
-        "8", "9", "A", "B", "C", "D", "E", "F"
-    ];
-
-    public function __construct($private_key = null)
+    public function __construct()
     {
         $this->_app = \Liten\Liten::getInstance();
     }
@@ -85,9 +69,14 @@ class Cookies
     public function set($key, $value)
     {
         $value = $this->buildCookie($value);
-        // @formatter:off
         return setcookie(
-            $key, $value, time() + $this->_app->config('cookies.lifetime'), $this->_app->config('cookies.path'), $this->_app->config('cookies.domain'), $this->_app->config('cookies.secure'), $this->_app->config('cookies.httponly')
+            $key,
+            $value,
+            time() + $this->_app->config('cookies.lifetime'),
+            $this->_app->config('cookies.path'),
+            $this->_app->config('cookies.domain'),
+            $this->_app->config('cookies.secure'),
+            $this->_app->config('cookies.httponly')
         );
     }
 
@@ -100,37 +89,15 @@ class Cookies
      */
     public function remove($key)
     {
-        // @formatter:off
         return setcookie(
-            $key, '', time() - 86400, $this->_app->config('cookies.path'), $this->_app->config('cookies.domain'), $this->_app->config('cookies.secure'), $this->_app->config('cookies.httponly')
+            $key,
+            '',
+            time() - (86400 + $this->_app->config('cookies.lifetime')),
+            $this->_app->config('cookies.path'),
+            $this->_app->config('cookies.domain'),
+            $this->_app->config('cookies.secure'),
+            $this->_app->config('cookies.httponly')
         );
-    }
-
-    /**
-     * @return string generated MAC address
-     */
-    public function generateMacAddress()
-    {
-        $vals = $this->_mac_address_vals;
-        if (count($vals) >= 1) {
-            $mac = array("00"); // set first two digits manually
-            while (count($mac) < 6) {
-                shuffle($vals);
-                $mac[] = $vals[0] . $vals[1];
-            }
-            $mac = implode(":", $mac);
-        }
-        return $mac;
-    }
-
-    /**
-     * Make sure the provided MAC address is in the correct format
-     * @param string $mac
-     * @return bool true if valid; otherwise false
-     */
-    public function validateMacAddress($mac)
-    {
-        return (bool) preg_match("/^" . $this->_valid_mac . "$/i", $mac);
     }
 
     /**
@@ -179,7 +146,7 @@ class Cookies
         $mac = sprintf("exp=%s&data=%s", urlencode($vars['exp']), urlencode($vars['data']));
         $key = hash_hmac($this->_app->config('cookies.crypt'), $mac, $this->_app->config('cookies.secret.key'));
 
-        if ($vars['digest'] != $key) {
+        if (!hash_equals($vars['digest'], $key)) {
             // The cookie has been compromised
             return false;
         }
