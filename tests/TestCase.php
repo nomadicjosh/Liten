87970102d6171c273e19561a16bb13719f20c4c0
Joshua Parker <joshmac3@icloud.com>
2016-10-03 00:53:44 -0400

    Added Travis CI.

---
+++ tests/TestCase.php
@@ -0,0 +1,29 @@
+<?php
+namespace Tests;
+
+use \Liten;
+
+class RouterTest extends \PHPUnit_Framework_TestCase
+{
+    public function testMapRouting() {
+        $app = \Liten\Liten();
+        
+        $app->get('/foo/', function () {
+            return 'get foo';
+        });
+        
+        $app->post('/foo/', function () {
+            return 'post foo';
+        });
+        
+        $app->delete('/foo/', function () {
+            return 'delete foo';
+        });
+        
+        $app->put('/foo/', function () {
+            return 'put foo';
+        });
+        
+        $app->run();
+    }
+}
